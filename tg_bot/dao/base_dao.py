af47b9aa19f53f2b753a59ea4ad686938d1cba20
damotsov <george.damotsov@gmail.com>
2025-04-09 21:23:33 +0300

    fix keyboard handler

---
+++ tg_bot/dao/base_dao.py
@@ -0,0 +1,25 @@
+
+
+
+class BaseDAO:
+    model = None
+
+
+    @classmethod
+    async def add(cls,id: int):
+        pass
+
+
+    @classmethod
+    async def get(cls, id: int):
+        pass
+
+
+    @classmethod
+    async def update(cls,id: int):
+        pass 
+
+
+    @classmethod
+    async def delete(cls,id: int):
+        
